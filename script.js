815436019b4d66efafd0f1b1760b3278bb9b433f
CNeilas <humanmusics@gmail.com>
2024-05-23 21:31:19 +0200

    v3

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const addBtn = document.getElementById("add-coffee");
 const coffeeNumber = document.getElementById("coffee-number")
 const totalNumber = document.getElementById("total-number")
+const resetBtn = document.getElementById("reset")
 
 let coffeeCount = JSON.parse(localStorage.getItem("count")) || 0;
 coffeeNumber.innerText = coffeeCount;
@@ -8,7 +9,7 @@ coffeeNumber.innerText = coffeeCount;
 let total = JSON.parse(localStorage.getItem("total")) || 0;
 totalNumber.innerText = total;
 
-
+let lastReset = JSON.parse(localStorage.getItem("lastReset"))
 
 function addCoffee() {
     coffeeCount++
@@ -18,16 +19,30 @@ function addCoffee() {
 
 
 function resetCounts() {
-    const date = new Date()
-    if (date.getHours() === 0  && date.getMinutes() ===0) {
-        total += coffeeCount
-        localStorage.setItem("total", JSON.stringify(total))
-        coffeeCount = 0
-        localStorage.removeItem("count")
-        coffeeNumber.innerText = coffeeCount
-        totalNumber.innerText = total
+    const date = new Date();
+    const currentDate = date.toISOString().split("T")[0];
+
+    if (!lastReset || new Date(lastReset) < new Date(currentDate)) {
+        total += coffeeCount;
+        localStorage.setItem("total", JSON.stringify(total));
+        coffeeCount = 0;
+        localStorage.removeItem("count");
+        coffeeNumber.innerText = coffeeCount;
+        totalNumber.innerText = total;
+        lastReset = currentDate;
+        localStorage.setItem("lastReset", JSON.stringify(lastReset));
     }
 }
 
-addBtn.onclick = addCoffee
-setInterval(resetCounts, 60000)
+function reset() {
+    coffeeCount = 0;
+    localStorage.removeItem("count");
+    coffeeNumber.innerText = coffeeCount;
+}
+
+resetCounts()
+
+
+addBtn.onclick = addCoffee;
+resetBtn.onclick = reset;
+setInterval(resetCounts, 60000);
